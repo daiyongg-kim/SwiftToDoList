48e96fa66ece78dc8bcccb76c81136aec7f81909
Daiyong Kim <daiyongg.kim@gmail.com>
2026-04-10 18:41:48 -0700

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Daiyong Kim on 2026-04-10.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
